36679ef8a6bb43a3354df90334e110980925f3bb
nidsishah <nidsishah94@gmail.com>
2015-07-11 20:00:18 +0530

    php gng

--- add_partner.php
+++ add_partner.php
@@ -1,47 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>New document</title>
-	<meta name="generator" content="TSW WebCoder 2013">
+	<title>Partner</title>
+	
 </head>
 <body>
 
 <?php
+echo "hii";
 if(isset($_POST['submit']))
 {
+	echo "hii";
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
+
 $conn = mysql_connect($dbhost, $dbuser, $dbpass);
 if(! $conn )
 {
   die('Could not connect: ' . mysql_error());
 }
-mysql_select_db('combo');
+echo "hii";
+
+$db=mysql_select_db('school_selection');
+	if(!$db)
+	{
+		echo "db nt selected";
+	}
 
 //if(! get_magic_quotes_gpc() )
 //{
-   $txt_partner = addslashes ($_POST['txt_partner']);
-   $crime = addslashes ($_POST['crime']);
-   $date = addslashes ($_POST['date']);
-   $status = addslashes ($_POST['status']);
-   $c_id = addslashes ($_POST['c_id']);
-   $address = addslashes ($_POST['address']);
-   $contact = addslashes ($_POST['contact_no']);
-   $background = addslashes ($_POST['background']);
-   //$photo = addslashes ($_POST['photo']);
-   echo "hey";
-   $punishment = addslashes ($_POST['punishment']);
+   $partner_name = addslashes ($_POST['partner_name']);
+   $partner_password = addslashes ($_POST['partner_password']);
+   $partner_credits = addslashes ($_POST['partner_credits']);
+   $partner_area = addslashes ($_POST['partner_area']);
+   $sql = "INSERT INTO users ".
+       "(Username,Password,Credits,Area) ".
+       "VALUES('$partner_name','$partner_password','$partner_credits','$partner_area' )";
+   $retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not enter data: ' . mysql_error());
+}
+else
+	{
+		echo "data entered successsfully";
+		
+	}
+echo "Entered data successfully\n";	   
+ 
+mysql_close($conn);
+}
+else
+{
    ?>
    
-<form name="addLocalP" method="post" action="<?php $_PHP_SELF ?>">
+<form  method="post" action="<?php $_PHP_SELF ?>">
 	Partner Name : <input type="text" name="partner_name" value=""><br>
 	Password : <input type="password" name="partner_password" value=""><br>
 	Credits :  <input type="text" name="partner_credits" value=""><br>
 	Area : <input type="text" name="partner_area" value=""><br>
-		<input type="Submit" name="btnSub_addP" value="Enter">
+		<input type="submit" name="submit" value="Enter">
 </form>
 
-
+<?php
+}
+?>
 </body>
 </html>
